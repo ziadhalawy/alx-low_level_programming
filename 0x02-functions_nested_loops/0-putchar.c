c124657667ac57cea6eb806ec726162211842334
zeiad <z.halawy@gmail.com>
2023-07-19 00:30:06 +0300

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /*
  * program to print a _putchar with new line
  */
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+
+/ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int _putchar(char);
+}
+#endif
