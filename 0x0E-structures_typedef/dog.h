e9d0bc4c8803a0c2219af3de287e1c51007a1c13
zeiad <z.halawy@gmail.com>
2023-08-17 00:58:41 +0300

    Done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,14 @@
-#ifndef dog_h
-#define dog_h
+#ifndef DOG_H
+#define DOG_H
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-dog_t *new_dog(char *name, float age, char *owner);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
